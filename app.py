e9f93a3bc26fadbb90d4ad0ac28737be8b270215
Ubuntu <ubuntu@ip-172-31-84-89.ec2.internal>
2019-06-08 15:31:26 +0000

    Added form, submit and insert functionality

--- app.py
+++ app.py
@@ -23,12 +23,29 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     # Redirect to existing tasks.html template and will return everything in our tasks collection
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    # Create variables and pass that in to the render_template() method
+    _tasks = mongo.db.tasks.find()
+    task_list = [task for task in _tasks]
+    return render_template("tasks.html", tasks=task_list)
 # 11. Create new route decorator and add_task function
 # Allows user to add a single task
 @app.route('/add_task')
 def add_task():
-    return render_template("addtask.html")
+    # 12. Add categories from the database to the render_template() method
+    # Create variables and pass that in to the method
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("addtask.html", categories=category_list)
+# 13. Create new route decorator to insert task when form is submitted
+@app.route('/insert_task', methods=["POST"])
+def insert_task():
+    # 14. Add function to insert tasks to databases
+    # Variables to store the task collection
+    tasks = mongo.db.tasks
+    # Insert the form request to the DB as a dictionary
+    tasks.insert_one(request.form.to_dict())
+    # Once submitted, redirect the user to the tasks.html file
+    return redirect(url_for('get_tasks'))
 
 ''' 3. Create test function with the default route which will display some text as a proof of concept
 DELETE THIS WHEN COMPLETING STEP 8
--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    
-    //Collapsible command for the accordion
-    $('.collapsible').collapsible();
-});
